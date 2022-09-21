13a2ad6ccce05e35ce029846ed744532261f8715
MPilecarte <mayara.pilecarte@gmail.com>
2022-09-20 22:34:47 -0300

    Fix: 'lateinit' ; Feature: 'resultText'

--- app/src/main/java/com/example/dice_roller/MainActivity.kt
+++ app/src/main/java/com/example/dice_roller/MainActivity.kt
@@ -2,23 +2,35 @@ package com.example.dice_roller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import android.widget.Button
 import android.widget.TextView
+import com.example.dice_roller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var Binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton:Button =findViewById(R.id.RollButton)
+//   ou  val rollButton:Button = viewBinding(R.id.RollButton)
         rollButton.setOnClickListener { rollDice() }
+//        Evento listener executa a função quando o botão for clicado
     }
 
     fun rollDice(){
+
+        val randomInt = (1..100).random().toString() // converte o random (que é do tipo Int) em uma String para ser exibida na caixa de texto
+
         val resultText: TextView = findViewById(R.id.RollButton)
+//     declarando uma variavel de nome 'resultText' com ?classe? TextView e buscando no layout
 
-        val randomInt = (1..6).random().toString()
+//       Ou binding.<nome da caixa de text>
+        resultText.text= randomInt
 
+//        jogando o texto do layout na variavel 'resultText'
     }
 
 }
