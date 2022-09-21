10c8fa3791b2a153159e36fd177b31d9f06c3440
MPilecarte <mayara.pilecarte@gmail.com>
2022-09-20 21:21:55 -0300

    Feature: fun rollDice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = viewBinding(R.id.RollButton)
+        rollButton.setOnClickListener { rollDice() }
     }
+
+    fun rollDice(){
+        val randomInt = (1..6).random().toString()
+        val resultText: TextView = findViewById(R.id.RandomNumber)
+    }
+
+
 }
